94b9119d0ff6f4077d7d6efa0231bcf0986e3eeb
adilahamad786 <adilahamad.personally@gmail.com>
2023-05-18 18:40:02 +0530

    Create basic express server.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const connectDb = require("./source/config/db");
+const express = require("express");
+const userRouter = require("./source/routers/user");
+const itemRouter = require("./source/routers/item");
+require("dotenv").config();
 
-connectDb()
+const app = express();
+
+// Connect to mongodb
+connectDb();
+const port = process.env.PORT || 8800;
+
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/item", itemRouter);
+
+app.get("/", (req, res) => {
+    res.send("This is home page!");
+})
+
+app.get("*", (req, res) => {
+    res.send("Page not found!");
+})
+
+app.listen(port, () => console.log("Server is running on port : ", port))
